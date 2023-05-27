4f9ca137907efd9bbba1644a137a7a60d4295e5e
Daniel Maslowski <info@orangecms.org>
2023-05-27 17:33:26 +0200

    add cargo config

--- apps/blink-rs/build.rs
+++ apps/blink-rs/build.rs
@@ -1,5 +1,5 @@
 fn main() {
     //println!("cargo:rustc-link-arg-bins=-T../../../tkey-libs/app.lds");
-    println!("cargo:rustc-link-arg=-T../../../tkey-libs/app.lds");
+    //  println!("cargo:rustc-link-arg=-T../../../tkey-libs/app.lds");
     println!("cargo:rustc-link-arg=--export-dynamic-symbol=main");
 }
